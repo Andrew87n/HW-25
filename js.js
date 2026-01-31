fb42d2e01bbcce26400a129e8281ee88175ac915
Andrew87n <andlop@ukr.net>
2026-01-31 13:04:46 +0200

    1234

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
+import { increase, decrease } from "./modules/counter";
+import { updateValue } from "./modules/utils";
+
 const valueEl = document.getElementById("value");
 const incBtn = document.getElementById("increase");
 const decBtn = document.getElementById("decrease");
-
-import { increase, decrease } from "./modules/counter";
-import { updateValue } from "./modules/utils";
 
 incBtn.addEventListener("click", () => {
   const value = increase();
@@ -14,4 +14,3 @@ decBtn.addEventListener("click", () => {
   const value = decrease();
   updateValue(valueEl, value);
 });
-console.log("JS працює");
